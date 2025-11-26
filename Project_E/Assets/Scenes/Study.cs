4ab6f6d7153358405b7e3c6cef15e1455799e9bf
JeonHyeongChan <glaredpanda@gmail.com>
2025-11-26 16:48:34 +0900

    [Add] Hello,World!

---
+++ Project_E/Assets/Scenes/Study.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+public class Study: MonoBehaviour
+{ //public 전역
+  //private 지역
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
